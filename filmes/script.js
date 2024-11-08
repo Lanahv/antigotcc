25871941a6b4dc0012ee3621487394cb1fb692c5
Lanahv <alanavieira410@gmail.com>
2024-11-08 09:54:16 -0300

    uppp

--- filmes/script.js
+++ filmes/script.js
@@ -1,10 +1,12 @@
-//menu responsivo
+//MENU RESPONSIVO
 const menuBtn = document.querySelector(".menu");
 const navigation = document.querySelector(".navigation");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("show"); // Alterna a classe show
-  });
+});
+  
+
   
 
--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const navigation = document.querySelector(".navigation");
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("show"); // Alterna a classe show
-  });
+});
   
 
 
